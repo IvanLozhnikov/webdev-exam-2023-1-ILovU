8f49b05c7f334fb8f331dd3a4a0358160ca72b73
IvanLozhnikov <113699224+IvanLozhnikov@users.noreply.github.com>
2024-01-12 21:39:48 +0300

    Добавлена кнопка удалить
    Кнопка Отправляет запрос на сервер Но изменений нет

--- exm/index1.js
+++ exm/index1.js
@@ -9,12 +9,9 @@ function saveToSessionStorage(data) {
 let getRequestMainTable = async function (url, method) {
     if (method == 'GET') {
         return await fetch(url, {
-
         });
-
     }
 };
-
 //   ДЛЯ ОПРЕДЕЛЕНИЯ ГОД/ЛЕТ/ГОДА
 function plural(number, titles) {
     let cases = [2, 0, 1, 1, 1, 2];
@@ -35,14 +32,15 @@ function fillTable(data) {
         let tr = clonedRow.querySelector("#id-tr");
         tr.setAttribute('data-id', `${JSON.stringify(data[i].id)}`);
         let route = clonedRow.querySelector('.route');
-        let desc = clonedRow.querySelector('.desc');
-        let mainObjects = clonedRow.querySelector('.mainObjects');
-        let SecondObjects = clonedRow.querySelector('.SecondObjects');
+        let person = clonedRow.querySelector('.person');
+        let GidName = clonedRow.querySelector('.GidName');
+        let StudentName = clonedRow.querySelector('.StudentName');
 
-        desc.textContent = data[i].persons;
-        mainObjects.textContent = data[i].guide_id;
-        SecondObjects.textContent = data[i].student_id;
         route.textContent = data[i].route_id;
+        person.textContent = data[i].persons;
+        GidName.textContent = data[i].guide_id;
+        StudentName.textContent = data[i].student_id;
+
 
         tableBody.appendChild(clonedRow);
     }
@@ -124,8 +122,7 @@ function getMainTable() {
             return data.json();
         })
         .then(data => {
-            parseData(data); addHint(data);
-
+            parseData(data); 
             saveToSessionStorage(data);
         }
         )
@@ -157,7 +154,7 @@ function searchFromBtn() {
         }
     }
 }
-// ------------------------------------------------Кнопка для выборанного маршрута----------------------------
+// ------------------------------------------------Кнопка для выбранного Заказа----------------------------------------------------
 let chosenRoute = new Set();
 document.querySelector('.fillbody').addEventListener('click', function (event) {
     // Проверка, что клик был по кнопке
@@ -167,35 +164,29 @@ document.querySelector('.fillbody').addEventListener('click', function (event) {
 
         // Получение значения атрибута data-id из строки
         let dataId = row.getAttribute('data-id');
-
-
-        //console.log('Кнопка в строке с data-id ' + dataId + ' была нажата.');
-        if (chosenRoute.has(dataId)) {
-            DeleteGid(dataId, 'deluseroute');
-            chosenRoute.delete(dataId);
-
-            if (chosenRoute.size == 0) {
-                let delElem = document.querySelector('.chosen-gids');
-                delElem.classList.remove('d-none');
-
-
+        document.querySelector('.choose').addEventListener('click', function (event) {
+            // Проверка, что клик был по кнопке
+            if (event.target.tagName === 'BUTTON') {
+        let url = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/orders?api_key=${apiKey}`;
+        let res = requests(url, dataId, "DELETE");
+        res.then(data => {
+            console.log(data);
+            let key = "error"
+            let isErr = key in data;
+            if (isErr) {
+                msgFromServer(`${data.error}`, "alert-danger");
+                document.querySelector(".close").click();
+            } else {
+                
+                msgFromServer("Заявка успешно оправлено", "alert-success");
+                document.querySelector(".close").click();
             }
-            //console.log(chosenRoute);
-
-        } else {
-            chosenRoute.add(dataId);
-            if (chosenRoute.size > 0) {
-                let delElem = document.querySelector('.chosen-gids');
-                delElem.classList.add('d-none');
-
-
+    
+        });
+            //console.log(dataId);
             }
-            
-            //console.log(chosenRoute);
-        }
-
+        })
     }
-
 });
 // ------------------------ВСЕ ЧТО СВЯЗАНО С ОТПРАВКОЙ ФОРМЫ НА СЕРВЕР + ОБЩАЯ ФУНКЦИЯ ДЛЯ ВСЕХ ТИПОВ ЗАПРОСА---------------------------------
 function msgFromServer(msg, alertType) {
@@ -242,9 +233,12 @@ async function requests(url, data, method) {
         return await fetch(url, {
             method: method,
             body: data
+            
         })
             .then(Response => Response.json())
-            .catch((e) => console.log("Error in " + method));
+            .catch(error => {
+                console.error('Ошибка при выполнении запроса:', error.message);
+                msgFromServer(`Ошибка при выполнении запроса: ${error.message}`, "alert-danger");});
     }
 }
 // ----------------------------------------MODAL WINDOW TO SEND-------------------------------------------------------
@@ -258,109 +252,7 @@ function getRouteNameById(data) {
         }
     }
 }
-
-
-//--------------------------------------------------УСЛОВИИ ДЛЯ МОДАЛЬНОГО ОКНА ----------------------------------------------------------
-function conditionOfModalForm() {
-    //---------------DATE-------------------
-    function addDays(date, days) {
-        let newDate = new Date(date);
-        newDate.setDate(newDate.getDate() + days);
-        return newDate;
-    }
-
-    function addMonths(date, months) {
-        let newDate = new Date(date);
-        newDate.setMonth(newDate.getMonth() + months);
-        return newDate;
-    }
-
-    let dateInput = document.querySelector('.datepicker');
-    let tomorrow = addDays(new Date(), 1);
-    let tomorrowString = tomorrow.toISOString().split('T')[0];
-
-    let threeMonthsFromTomorrow = addMonths(tomorrow, 3);
-    let threeMonthsFromTomorrowString = threeMonthsFromTomorrow.toISOString().split('T')[0];
-
-    dateInput.setAttribute("value", tomorrowString);
-    dateInput.setAttribute("min", tomorrowString);
-    dateInput.setAttribute("max", threeMonthsFromTomorrowString);
-
-
-    // console.log(todayString, endDateString);
-
-    //---------------CLOCK-------------------
-}
-// ------------------------------------------------КНОПКА SENDREQUEST-----------------------------------------------------------
-
-document.querySelector(".sendRequest").addEventListener("click", () => {
-    let formData = new FormData();
-    let date = document.querySelector(".datepicker").value;
-    let hoursNumber = Number(document.querySelector('.hours-selected').value);
-    let countExcurs = Number(document.querySelector(".people-amount").value);
-    let price = Math.round(Number(document.querySelector(".excursion-price").textContent.split(" ")[0]));
-    let fioField = Number(document.querySelector('.gid-name').getAttribute("id"));
-    let routeField = Number(document.querySelector('.excursion-name').getAttribute("id"));
-    let selectedTime = document.querySelector(".excursion-start").value;
-
-
-   
-    console.log(`date: ${date}, type: ${typeof date}`);
-    console.log(`hoursNumber: ${hoursNumber}, type: ${typeof hoursNumber}`);
-    console.log(`countExcurs: ${countExcurs}, type: ${typeof countExcurs}`);
-    console.log(`price: ${price}, type: ${typeof price}`);
-    console.log(`fioField: ${fioField}, type: ${typeof fioField}`);
-    console.log(`routeField: ${routeField}, type: ${typeof routeField}`);
-    console.log(`Time: ${selectedTime}, type: ${typeof selectedTime}`);
-
-   
-    formData.append('date', date);
-    formData.append('duration', hoursNumber);
-    formData.append('guide_id', fioField);
-    formData.append('optionFirst', optionOne ? 1 : 0);
-    formData.append('optionSecond', optionTwo ? 1 : 0);
-    formData.append('persons', countExcurs);
-    formData.append('price', price);
-    formData.append('route_id', routeField);
-    formData.append('time', selectedTime);
-    let url = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/orders?api_key=${apiKey}`;
-    let res = requests(url, formData, "POST");
-    res.then(data => {
-        console.log(data);
-        let key = "error"
-        let isErr = key in data;
-        if (isErr) {
-            msgFromServer(`${data.error}`, "alert-danger");
-            document.querySelector(".close").click();
-        } else {
-            
-            msgFromServer("Заявка успешно оправлено", "alert-success");
-            document.querySelector(".close").click();
-        }
-
-    });
-
-
-
-
-
-  
-});
-
-// ------------------------------------------------КНОПКА SENDREQUEST----------------------------------------------------------- 
-document.querySelector('#start-time').addEventListener('input', function () {
-    let enteredTime = this.value;
-
-    if (/^([0-1]?[0-9]|2[0-3]):(00|30)$/.test(enteredTime)) {
-        
-        this.dataset.prevValidTime = enteredTime;
-    } else {
-        
-        this.value = this.dataset.prevValidTime || '';
-    }
-});
 window.onload = function () {
     getMainTable();
-    conditionOfModalForm();
 
 };
